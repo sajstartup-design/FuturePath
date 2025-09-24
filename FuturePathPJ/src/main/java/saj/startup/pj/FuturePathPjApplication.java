aaf3128c62926c81a7b516540fb88e17c387e313
Julius_Basas <juliusb@cyzennt.co.jp>
2025-09-24 09:24:25 +0800

    Initial Files again

---
+++ FuturePathPJ/src/main/java/saj/startup/pj/FuturePathPjApplication.java
@@ -0,0 +1,13 @@
+package saj.startup.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturePathPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturePathPjApplication.class, args);
+	}
+
+}
---
+++ FuturePathPJ/src/test/java/saj/startup/pj/FuturePathPjApplicationTests.java
@@ -0,0 +1,13 @@
+package saj.startup.pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturePathPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
