0c58f5f23be06f9bcd5ada80de4944f9fe45fb75
Julius_Basas <juliusb@cyzennt.co.jp>
2025-09-24 09:29:00 +0800

    Login Controller and Html

---
+++ FuturePathPJ/src/main/java/saj/startup/pj/controller/LoginController.java
@@ -0,0 +1,14 @@
+package saj.startup.pj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String showLogin() {
+		
+		return "login";
+	}
+}
