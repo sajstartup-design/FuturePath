74e9bb4c326c5b4af0cb469ea1beaaf4a311ac85
julius <jusbas0713@gmail.com>
2025-10-01 01:10:15 +0800

    Sidebar active and hovering

--- FuturePathPJ/src/main/java/saj/startup/pj/controller/DashboardController.java
+++ FuturePathPJ/src/main/java/saj/startup/pj/controller/DashboardController.java
@@ -1,13 +1,16 @@
 package saj.startup.pj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
 	@GetMapping("/admin/dashboard")
-	public String showAdminDashboard() {
+	public String showAdminDashboard(Model model) {
+		
+		model.addAttribute("page", "dashboard");
 		
 		return "dashboard/dashboard";
 	}
