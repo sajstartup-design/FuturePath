e6dec5cb6d8b3a1185c06c324bc9266d4ce48317
Julius_Basas <juliusb@cyzennt.co.jp>
2025-10-01 12:52:59 +0800

    User success msg and error msg

---
+++ FuturePathPJ/src/main/java/saj/startup/pj/common/MessageConstant.java
@@ -0,0 +1,8 @@
+package saj.startup.pj.common;
+
+public class MessageConstant {
+
+	public static final String USER_ADDED = "A new user hs been added";
+	
+	public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+}
