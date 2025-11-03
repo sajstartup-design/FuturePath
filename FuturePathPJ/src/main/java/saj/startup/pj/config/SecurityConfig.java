d15e226cb0e07a8e8ac2ab0a16233c4a6288b77b
Julius_Basas <juliusb@cyzennt.co.jp>
2025-11-03 10:57:27 +0800

    Add /register mapping to be accessible

--- FuturePathPJ/src/main/java/saj/startup/pj/config/SecurityConfig.java
+++ FuturePathPJ/src/main/java/saj/startup/pj/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
 						.requestMatchers("/script/**").permitAll()
 						.requestMatchers("/fonts/**").permitAll()
 						.requestMatchers("/gif/**").permitAll()
+						.requestMatchers("/register").permitAll()
 						
 						.requestMatchers("/admin/**").hasAnyAuthority(CommonConstant.ROLE_ADMIN)
 						.requestMatchers("/api/admin/**").hasAnyAuthority(CommonConstant.ROLE_ADMIN)
